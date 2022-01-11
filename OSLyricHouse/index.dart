9f1c658c4dee97402b942bab80e1857d87367049
solgon-network <theo.gonzales719@gmail.com>
2022-01-11 14:42:56 -0700

    Deletion of main.dart project.

--- OSLyricHouse/index.dart
+++
@@ -1,8 +0,0 @@
-export 'OSLyricHouse_bloc.dart';
-export 'OSLyricHouse_event.dart';
-export 'OSLyricHouse_model.dart';
-export 'OSLyricHouse_page.dart';
-export 'OSLyricHouse_provider.dart';
-export 'OSLyricHouse_repository.dart';
-export 'OSLyricHouse_screen.dart';
-export 'OSLyricHouse_state.dart';
